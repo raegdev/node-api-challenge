d1de0ff7bcfda3550d173e2412b9f66654ec1c69
rae-glazier <glazier.home.life@gmail.com>
2020-04-19 20:36:52 -0500

    actions started

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -2,5 +2,22 @@ const express = require('express')
 const router = express.Router()
 const action = require('../data/helpers/actionModel')
 
+//id (number)
+//project_id (number)(required)
+//description (string)(required, 128 char)
+//notes (string) (required)
+// completed (boolean)
 
+router.get("/", (req,res) => {
+    action.get()
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                message: "no actions available"
+            })
+        })
+})
 module.exports = router 
