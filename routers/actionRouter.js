6b5e51a0087d0edf4c020093a5bd1f4bc93523bf
rae-glazier <glazier.home.life@gmail.com>
2020-04-19 20:39:58 -0500

    get id action complete

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -8,7 +8,7 @@ const action = require('../data/helpers/actionModel')
 //notes (string) (required)
 // completed (boolean)
 
-router.get("/", (req,res) => {
+router.get("/", (req,res) => { //get actions
     action.get()
         .then(action => {
             res.status(200).json(action)
@@ -17,6 +17,25 @@ router.get("/", (req,res) => {
             console.log(error)
             res.status(500).json({
                 message: "no actions available"
+            })
+        })
+})
+
+router.get("/:id", (req,res) => { //get spec action
+    action.get(req.params.id)
+        .then(act => {
+            if (act) {
+                res.status(200).json(act)
+            } else {
+                res.status(404).json({
+                    message: "no action at this id"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "error retrieving action"
             })
         })
 })
